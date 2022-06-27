c23e064bd87f8479b203f3921cd3dc9c9aa9d339
perazaf1 <pe.razafindrakoto@gmail.com>
2022-06-27 16:28:50 +0200

    commit loader

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 const hello = document.getElementById("intro")
+var loader = document.getElementById('preloader')
+
+window.addEventListener('load', function(){
+    loader.style.display = "none"
+})
